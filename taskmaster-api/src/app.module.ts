ddc2a0d90138fe2384b2ff2d91fa40f85d710a9d
nicolasmedrado <nicozaum@gmail.com>
2026-01-23 20:59:26 -0400

    chore: add mongodb with docker compose and connect api

--- taskmaster-api/src/app.module.ts
+++ taskmaster-api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
@@ -7,11 +8,18 @@ import { HealthModule } from './health/health.module';
 @Module({
   imports: [
     HealthModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('MONGO_URI');
+        if (!uri) throw new Error('MONGO_URI não definida no ambiente (.env)');
+        return { uri };
+      },
+    }),
     ConfigModule.forRoot({
-      // Torna o módulo global, dispensando importação em outros módulos
-      isGlobal: true, 
-      // Caminho opcional para o arquivo .env (padrão é a raiz do projeto)
-      envFilePath: '.env', 
+      isGlobal: true,
+      envFilePath: '.env', // ajuste se seu .env não estiver na raiz do serviço
     }),],
   controllers: [AppController],
   providers: [AppService],
